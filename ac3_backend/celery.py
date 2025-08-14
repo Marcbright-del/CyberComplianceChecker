2423c209a7133933b922ec8baf6acd107ba95aaa
Marcbright-del <ngefacbrightmarc@gmail.com>
2025-08-14 14:20:21 +0100

    Fix: Remove final Celery config for deployment

--- ac3_backend/celery.py
+++
@@ -1,8 +0,0 @@
-# ac3_backend/celery.py
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ac3_backend.settings')
-app = Celery('ac3_backend')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
